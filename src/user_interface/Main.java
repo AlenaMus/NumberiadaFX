9030c42af55d14075ee7cc3d5bdcb2eeb777a6c9
bbbergman <orbergman9@gmail.com>
2017-01-03 17:58:00 +0200

    update check

--- src/user_interface/Main.java
+++ src/user_interface/Main.java
@@ -1,13 +1,10 @@
 package user_interface;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+//update check
     public static void main(String[] args) {
         launch(args);
     }
